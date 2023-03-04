81c73b076821ee1c6b25559f96ec1d6eba4f1abb
Jesse Fernandez <96546829+00jferna@users.noreply.github.com>
2023-03-04 12:10:59 -0600

    Finished useState implementation

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 const UseState = () => {
+  console.log(useState("light"));
+  const [theme, setTheme] = useState("light");
   return (
-    <div>
+    <div className={theme}>
       <h1>UseState Component</h1>
+      <button onClick={() => setTheme("dark")}>Dark</button>
+      <button onClick={() => setTheme("light")}>Light</button>
+      <button
+        onClick={() =>
+          theme === "light" ? setTheme("dark") : setTheme("light")
+        }
+      >
+        Toggle1
+      </button>
+      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+        Toggle1
+      </button>
     </div>
   );
 };
